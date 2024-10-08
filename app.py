72818ce2d86681b089375e6b96d36bfa78dd835a
fricour <fricour@naturalsciences.be>
2024-10-08 11:49:51 +0200

    update taylor plot

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def create_target_diagram(df, add_annotations=False):
     fig, ax = plt.subplots(figsize=(10, 10))
     
     # Create the scatter plot
-    sns.scatterplot(data=df, x="NormalizedcRMSD", y='NormalizedBias', 
-                    hue='variable', s=150, ax=ax, legend=False)
+    plot = sns.scatterplot(data=df, x="NormalizedcRMSD", y='NormalizedBias', 
+                    hue='variable', style = 'setup', s=150, ax=ax, legend=True)
     
     # Add vertical and horizontal lines
     ax.axvline(0, color='black', linestyle='--', linewidth=0.5)
@@ -64,7 +64,7 @@ def create_target_diagram(df, add_annotations=False):
     
     # Add circular guidelines
     for radius in [0.5, 1]:
-        circle = plt.Circle((0, 0), radius, color='b', fill=False, 
+        circle = plt.Circle((0, 0), radius, color='black', fill=False, 
                             linestyle='--' if radius == 0.5 else '-')
         ax.add_patch(circle)
     
@@ -74,13 +74,16 @@ def create_target_diagram(df, add_annotations=False):
                     xytext=(5, 5), textcoords='offset points')
     
     # Set plot limits
-    #ax.set_xlim([-2, 2])
-    #ax.set_ylim([-2, 2])
+    #ax.set_xlim([-3, 3])
+    #ax.set_ylim([-3, 3])
     
     # Add title and labels
     ax.set_title("Target Diagram", fontsize=16)
     ax.set_xlabel("Normalized cRMSD", fontsize=12)
     ax.set_ylabel("Normalized Bias", fontsize=12)
+
+    # Add legend
+    plot.legend(loc='center left', bbox_to_anchor=(1.05, 0.5), ncol=1)
     
     if add_annotations:
         annotations = [
@@ -108,7 +111,7 @@ app_ui = ui.page_fluid(
     ),
     ui.layout_sidebar(
         ui.sidebar(
-            ui.tags.h3("Simulation"),
+            ui.tags.h3("Simulation", style="font-weight: bold;"),
             ui.input_text("yaml_path", "Path to simulation.yaml", "/home/fricour/bcz1d/experiment/simulation.yaml"),
             ui.input_action_button("load_yaml", "Load YAML Configuration"),
             ui.output_text("yaml_load_status"),
@@ -118,7 +121,8 @@ app_ui = ui.page_fluid(
             ui.input_selectize("setups", "Setups", choices=[], multiple=True),
             ui.input_action_button("load_button", "Load NetCDF data"),
             ui.input_selectize("variables", "All Variables", choices=[], multiple=True),
-            ui.tags.h3("Validation"),            
+            ui.tags.hr(style="border-top: 2px solid #ccc; margin-top: 20px; margin-bottom: 20px;"),
+            ui.tags.h3("Validation", style="font-weight: bold;"),            
             ui.input_selectize("yaml_variables", "Variables from YAML", choices=[], multiple=True)
         ),
         ui.navset_tab(
@@ -129,10 +133,21 @@ app_ui = ui.page_fluid(
                     height="calc(100vh - 100px)" # Adjust the 100px value as needed to account for the header
                 ),
             ),
-            ui.nav_panel("Validation",
-                ui.output_plot("global_taylor_diagram")
+            ui.nav_panel("Validation"),
+            ui.nav_panel("Taylor Diagram",
+                ui.card(
+                    ui.output_plot("plot_taylor_diagram"),
+                    height="calc(100vh - 100px)" # Adjust the 100px value as needed to account for the header
+                )
             ),
         ),
+        ui.tags.footer(
+            ui.tags.div(
+                "Made with ❤️ by Florian Ricour",
+                class_="footer-content"
+            ),
+            class_="footer"
+        )
     )
 )
 
@@ -150,7 +165,7 @@ def server(input, output, session):
             ui.update_text("simulation_name", value=yaml_config['simulation_name'])
             ui.update_selectize("stations", choices=list(yaml_config['stations'].keys()))
             ui.update_selectize("setups", choices=yaml_config['setups'])
-            ui.update_selectize("yaml_variables", choices=list(yaml_config['variable_mapping'].keys()))
+            ui.update_selectize("yaml_variables", choices=list(yaml_config['variable_mapping'].keys()), selected=list(yaml_config['variable_mapping'].keys()))
         else:
             config.set(None)
 
@@ -209,7 +224,7 @@ def server(input, output, session):
         return ""
 
     @render.plot
-    @debounce(1) # Debounce the plot generation to avoid multiple calls
+    @debounce(1) # Debounce (1 second) the plot generation to avoid multiple calls (when selecting the input variables)
     @reactive.event(input.variables, input.load_button)
     def plot_grid():
         if not datasets() or (not input.variables()):
@@ -234,7 +249,6 @@ def server(input, output, session):
         for row, var in enumerate(variables):
             for col, station in enumerate(stations):
                 ax = axes[row, col]
-                print(ax)
                     
                 for setup in setups:
                     for data_type in ['result', 'rates']:
@@ -261,24 +275,28 @@ def server(input, output, session):
     
     @render.plot
     @reactive.event(input.yaml_variables)
-    def global_taylor_diagram():
+    def plot_taylor_diagram():
         if not input.yaml_variables() or not input.setups():
             return plt.figure()  # Return an empty figure if no variables are selected
         try:
-            # Construct the correct path to the data_taylor.arrow file
-            print(input.setups())
-            setup = input.setups()[0]  # Only use the first setup # NOTE: This is a temporary solution
-            file_path = os.path.join(input.result_dir(), input.simulation_name(), setup, "taylor_diagram_all_stations.arrow")
+            dfs = []
+            for i in range(len(input.setups())):
+                setup = input.setups()[i]
+                file_path = os.path.join(input.result_dir(), input.simulation_name(), setup, "taylor_diagram_all_stations.arrow")
         
-            # Read the feather file
-            df = pd.read_feather(file_path)
-        
+                # Read the feather file
+                df = pd.read_feather(file_path)
+                dfs.append(df)
+
+            # Concatenate the dataframes
+            df = pd.concat(dfs)
+
             # Filter the dataframe based on selected variables
             selected_vars = input.yaml_variables()
             filtered_df = df[df['variable'].isin(selected_vars)]
         
             # Create the target diagram
-            fig = create_target_diagram(filtered_df, add_annotations=True)
+            fig = create_target_diagram(filtered_df, add_annotations=False)
         
             return fig
         
